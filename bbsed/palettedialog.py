af0b7a4bab8cd511a7d319987b0a85a4f38f126a
Nate Duarte <slacknate@gmail.com>
2021-04-06 02:22:30 -0400

    [bbsed] add dialogs for editing
     - add dialog for showing and editing color palette data
     - add dialog for zooming in on sprites so it is easier
       to see individual pixels
     - implement support for changing colors in a palette via
       the palette dialog or by the sprite preview

---
+++ bbsed/palettedialog.py
@@ -0,0 +1,120 @@
+import io
+import traceback
+
+from PyQt5 import QtCore, Qt, QtWidgets
+
+from libhpl import convert_from_hpl, set_index_color
+
+from .ui.palettedialog import Ui_Dialog
+
+COLOR_BOX_SIZE = 15
+
+
+class PaletteDialog(QtWidgets.QDialog):
+
+    palette_data_changed = QtCore.pyqtSignal()
+
+    def __init__(self, parent=None):
+        flags = QtCore.Qt.WindowType.WindowTitleHint | QtCore.Qt.WindowType.CustomizeWindowHint
+        QtWidgets.QDialog.__init__(self, parent, flags=flags)
+
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        self.setWindowTitle("Palette Data")
+
+        self.palette_data = io.BytesIO()
+        self.palette_x = 0
+        self.palette_y = 0
+
+        # Set up our palette viewer with a scene.
+        # A scene can load a pixmap (i.e. an image like a PNG) from file or bytestring and display it.
+        self.palette_scene = QtWidgets.QGraphicsScene()
+        self.ui.png_view.setScene(self.palette_scene)
+
+        self.ui.png_view.mouseDoubleClickEvent = self.mouse_double_click_event
+        self.ui.png_view.mousePressEvent = self.mouse_click_event
+
+    def get_palette_img(self):
+        """
+        Get the palette image for conversion to an HPL file.
+        """
+        return self.palette_data
+
+    def _set_palette_index_color(self):
+        """
+        Pick a color and set it as the color for the current palette index.
+        If we successfully choose a color we modify the palette data and update the palette view,
+        as well as the sprite preview in the main window.
+        If the color dialog is cancelled no color data is modified.
+        """
+        dialog = QtWidgets.QColorDialog()
+        accepted = dialog.exec_()
+
+        if accepted:
+            qcolor = dialog.currentColor()
+            color_tuple = (qcolor.red(), qcolor.green(), qcolor.blue())
+            palette_index = (self.palette_x, self.palette_y)
+
+            try:
+                set_index_color(self.palette_data, palette_index, color_tuple)
+                self._update_png_view()
+                self.palette_data_changed.emit()
+
+            # FIXME: LOL
+            except Exception:
+                traceback.print_exc()
+                return
+
+    def mouse_double_click_event(self, _):
+        """
+        Event handler for double clicks on the palette view.
+        Picks the color for the current palette index.
+        """
+        # The palette index will be set via `mouse_click_event` before getting here.
+        self._set_palette_index_color()
+
+    def mouse_click_event(self, evt):
+        """
+        Event handler for single button press on the palette view.
+        Sets the palette index for `mouse_double_click_event` so the color picker can
+        set the color for the correct index.
+        """
+        x = evt.x()
+        y = evt.y()
+
+        # Convert the mouse event coordinates into an (x, y) palette index
+        # This is the (x, y) coordinate of our 256 color palette.
+        self.palette_x = x // COLOR_BOX_SIZE
+        self.palette_y = y // COLOR_BOX_SIZE
+
+    def set_palette_index(self, palette_index):
+        """
+        Set the current palette index manually and choose a color for it.
+        Used by the main window when the sprite view is double-clicked.
+        """
+        self.palette_x = palette_index[0]
+        self.palette_y = palette_index[1]
+        self._set_palette_index_color()
+
+    def _update_png_view(self):
+        """
+        Refresh the palette view so it is up to date with recent changes.
+        """
+        png_pixmap = Qt.QPixmap()
+        png_pixmap.loadFromData(self.palette_data.getvalue(), "PNG")
+
+        # Clear our image date and load in the updates image data.
+        self.palette_scene.clear()
+        self.palette_scene.addPixmap(png_pixmap)
+
+        # Ensure the graphics view is refreshed so our changes are visible to the user.
+        self.ui.png_view.viewport().update()
+
+    def set_palette(self, palette_path):
+        """
+        Load a palette from file and display the data in the dialog.
+        """
+        self.palette_data = io.BytesIO()
+        convert_from_hpl(palette_path, COLOR_BOX_SIZE, self.palette_data)
+        self._update_png_view()
---
+++ bbsed/zoomdialog.py
@@ -0,0 +1,63 @@
+import io
+
+from PyQt5 import QtCore, Qt, QtWidgets
+
+from .ui.zoomdialog import Ui_Dialog
+
+ZOOM_VIEW_SIZE = 247
+ZOOM_VIEW_CENTER = 124
+ZOOM_VIEW_SCALE = 5.0
+
+
+class ZoomDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        flags = QtCore.Qt.WindowType.WindowTitleHint | QtCore.Qt.WindowType.CustomizeWindowHint
+        QtWidgets.QDialog.__init__(self, parent, flags=flags)
+
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        self.setWindowTitle("Sprite Zoom")
+
+        self.current_sprite = io.BytesIO()
+        self.current_position = Qt.QPoint(0, 0)
+
+        self.zoom_scene = QtWidgets.QGraphicsScene()
+        self.ui.png_view.setScene(self.zoom_scene)
+
+        self.ui.png_view.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.ui.png_view.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+
+        # Scale the view so we zoom in.
+        self.ui.png_view.scale(ZOOM_VIEW_SCALE, ZOOM_VIEW_SCALE)
+
+    def set_sprite(self, current_sprite):
+        """
+        Set the sprite that we are zooming in on.
+        """
+        self.current_sprite = current_sprite
+
+        png_pixmap = Qt.QPixmap()
+        png_pixmap.loadFromData(self.current_sprite.getvalue(), "PNG")
+
+        # Clear our image date and load in the updates image data.
+        self.zoom_scene.clear()
+        self.zoom_scene.addPixmap(png_pixmap)
+
+        # TODO: draw a crosshair at the center of the zoom view
+
+        # Ensure the graphics view is refreshed so our changes are visible to the user.
+        self.ui.png_view.viewport().update()
+
+        # Move the cursor. If this is the initial load we move to 0, 0.
+        # Otherwise we move the cursor to the last focused position.
+        self.move_cursor(self.current_position)
+
+    def move_cursor(self, point):
+        """
+        Change what part of the sprite we are zoomed in on.
+        Maintain a reference to the current center position so if
+        we reload the sprite data we can re-center on this position again in `set_sprite`.
+        """
+        self.ui.png_view.centerOn(point.x(), point.y())
+        self.current_position = point
