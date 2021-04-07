87b6a02482a08153b94323d20b0eaba6baad136f
Nate Duarte <slacknate@gmail.com>
2021-04-07 11:41:17 -0400

    [errordialog] implement a dialog for showing error messages

---
+++ bbsed/errordialog.py
@@ -0,0 +1,49 @@
+import traceback
+
+from PyQt5 import QtCore, QtWidgets
+
+from .ui.errordialog_ui import Ui_Dialog
+
+
+def format_exc(exc_info):
+    """
+    Format exception info. This is expected to be the tuple returned by sys.exc_info().
+    """
+    return "".join(traceback.format_exception(*exc_info)).strip()
+
+
+class ErrorDialog(QtWidgets.QDialog):
+    def __init__(self, title, message, exc_info, parent=None):
+        QtWidgets.QDialog.__init__(self, parent, flags=QtCore.Qt.WindowType.WindowTitleHint)
+
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+        self.setWindowTitle(title)
+
+        # TODO: get icons into the project
+        self.ui.icon.hide()
+        # pixmap = QtGui.QPixmap("<icon file>")
+        # self.ui.icon.setPixmap(pixmap)
+
+        self.ui.message.setText(message)
+        self.ui.traceback.setText(format_exc(exc_info))
+        self.ui.traceback.hide()
+
+        # Setup UI elements for user interaction.
+        self.ui.toggle_tb.clicked.connect(self.toggle_tb)
+        self.ui.ok_button.clicked.connect(self.close)
+
+        # Resize the dialog to fit the displayed contents.
+        self.adjustSize()
+
+    def toggle_tb(self):
+        """
+        Our toggle button has been clicked.
+        If the traceback is shown, hide it. If the traceback is hidden, show it.
+        We need to resize the dialog after this operation so we do not have blank space
+        or inadvertently create a scroll area.
+        """
+        new_state = not self.ui.traceback.isHidden()
+        self.ui.traceback.setHidden(new_state)
+        self.adjustSize()
